23a9a81416b3abbbe91f669106301e5386fcce86
seohee <seohee@MacBook-Pro.local>
2020-11-22 14:30:49 +0900

    export sort complete

--- cmd_execute/cmd_export.c
+++ cmd_execute/cmd_export.c
@@ -27,7 +27,7 @@ int			    ft_strcmp(const char *s1, const char *s2)
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
 
-void sort_env(t_minishell *minishell, t_env *export_list)
+void sort_env(t_minishell *minishell)
 {
 	int i;
 	int j;
@@ -39,11 +39,10 @@ void sort_env(t_minishell *minishell, t_env *export_list)
 	while (i)
 	{
 		j = 0;
-		curr_node = export_list;
+		curr_node = minishell->export_list;
 		prev_node = NULL;
 		while (j < i && curr_node)
 		{
-		// ft_printf("%d %d %s\n",i,j,curr_node->variable);
 			if (ft_strcmp(curr_node->variable, curr_node->next->variable) > 0)
 			{
 				next_node = curr_node;
@@ -52,6 +51,8 @@ void sort_env(t_minishell *minishell, t_env *export_list)
 				curr_node->next = next_node;
 				if (prev_node != NULL)
 					prev_node->next = curr_node;
+				else if (prev_node == NULL)
+					minishell->export_list = curr_node;
 			}
 			prev_node = curr_node;
 			curr_node = curr_node->next;
@@ -59,13 +60,17 @@ void sort_env(t_minishell *minishell, t_env *export_list)
 		}
 		i--;
 	}
-	//print_export(minishell->export_list);
-
 }
 
 void envadd_back(t_env *list, char **newenv)
 {
 
+
+}
+
+int ft_exportcmp(t_minishell *minishell, char **split_new_env)
+{
+	// export_list에 curr->option의 앞부분과 일치하는 변수가 있는지 판별하고 치환하는 함수
 
 }
 
@@ -78,28 +83,29 @@ int cmd_export(t_cmd *curr, t_minishell *minishell)
 	{	
 		minishell->export_list = (t_env *)malloc(sizeof(t_env));
 		copy_env(minishell, minishell->export_list, minishell->env_list);
-		ft_printf("----------------------------절취선1 \n");
-		sort_env(minishell, minishell->export_list);
-		ft_printf("----------------------------절취선2 \n");
+		sort_env(minishell);
 	}
 	if (curr->argc == 1)
-		//print_export(minishell->export_list);
-	/*if (curr->argc > 1)
+		print_export(minishell->export_list);
+	if (curr->argc > 1)
 	{
+		//export
+		//a=a b=b c=c
 		new_env = ft_split(curr->option, ' ');
-		while(str)
+		while(new_env)
 		{
-			split_new_env = ft_split(*str, '=');
-			if (!(ft_strncmp 써서 minishell->export_list 에 curr->option의 앞부분과 일치하는 변수가 있는지 판별하고 치환하는 함수))
+			//a=a
+			split_new_env = ft_split(*new_env, '=');
+			if (!(ft_exportcmp(minishell, split_new_env)))
 			{
 				envadd_back(minishell->export_list, split_new_env);
 				envadd_back(minishell->env_list, split_new_env);
 				minishell->env_currnb++;
 			}
-			str++;
+			new_env++;
 			free_arr(split_new_env);
 		}
 		free_arr(new_env);
-	}*/
+	}
 	return (1);
 }
