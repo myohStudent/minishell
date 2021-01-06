a7d1a712f97ed4a6ff0a154110fb7356573d389a
myohStudent <myoh@student.42seoul.kr>
2021-01-06 11:48:27 +0900

    ft_printf norm completed

--- ft_printf/libft/ft_trim.c
+++ ft_printf/libft/ft_trim.c
@@ -3,62 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   ft_trim.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seohchoi <seohchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: myoh <myoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/22 22:02:52 by myoh              #+#    #+#             */
-/*   Updated: 2021/01/05 22:12:07 by seohchoi         ###   ########.fr       */
+/*   Updated: 2021/01/06 11:31:50 by myoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*trim_before(char *s)
+static	int	ft_delim(char c)
 {
-	if (s)
-	{
-		while (*s == '\t' || *s == ' ')
-			s++;
-	}
-	return (s);
-}
-
-int			trim_end(char *s)
-{
-	int		end;
-
-	end = ft_strlen(s);
-	end--;
-	if (s)
-	{
-		while (s[end] == '\t' || s[end] == ' ')
-			end--;
-	}
-	return (end + 1);
+	if (c == ' ' || c == '\n' || c == '\t')
+		return (1);
+	return (0);
 }
 
 char		*ft_trim(char *s)
 {
-	char	*res;
-	char	*trimmed;
-	int		len_trimmed;
+	char	*a;
 	int		i;
+	int		j;
+	int		l;
 
 	if (!s)
 		return (NULL);
-	s = trim_before(s);
-	len_trimmed = trim_end(s);
-	if ((trimmed = (char *)malloc(sizeof(char) * (len_trimmed + 1))) == NULL)
-		return (NULL);
-	res = trimmed;
 	i = 0;
-	while (i < len_trimmed)
-	{
-		if (!ft_isspace(s[i]))
-			*trimmed++ = s[i];
-		else if ((i - 1 >= 0) && (!ft_isspace(s[i - 1])))
-			(s[i] == '\t') ? (*trimmed++ = ' ') : (*trimmed++ = s[i]);
+	while (ft_delim(s[i]) == 1)
 		i++;
-	}
-	*trimmed = '\0';
-	return (res);
+	j = ft_strlen(s) - 1;
+	while (ft_delim(s[j]) == 1)
+		j--;
+	l = j - i + 1;
+	if (l < 0)
+		return (ft_strdup(""));
+	a = (char *)malloc(sizeof(char) * l + 1);
+	if (a == NULL)
+		return (NULL);
+	j = 0;
+	while (j < l)
+		a[j++] = s[i++];
+	a[j] = '\0';
+	return (a);
 }
